6b3f3d63bb1c18fd598f64e999e069e5b7f3a4d8
srijan-vaddadi <srijan.vaddadi@gmail.com>
2024-10-16 16:47:37 +0100

    basic login

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class UserInteraction
+{
+    public static void Main()
+    {
+        Console.Write("Would you like to login or terminate the program: ");
+        string action = Console.ReadLine();
+
+        if (action == "login")
+        {
+            Console.Write("Enter username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Do you know your password: ");
+            string passwordRemebered = Console.ReadLine();
+
+            if (passwordRemebered == "yes")
+            {
+                Console.Write("Enter password: ");
+                string password = Console.ReadLine();
+            }
+            else
+            {
+                ForgotPassword();
+            }
+        }
+    }
+
+    public static void ForgotPassword() { }
+}
